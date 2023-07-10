7c26219b0ecc272f19321ab758a5ca9e752e012c
nilsreich <nilsreich@gmail.com>
2023-07-10 10:47:01 +0200

    1 file modified, 1 deleted

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,54 @@
-<script lang='ts'>
-import Input from './Input.svelte'
-import List from './List.svelte'
-import Filter from './Filter.svelte'
+<script>
+  let value = '';
+  let search=''
+  let showAll=true
+
+let todos = []
+  function addTodo() {
+    if (value.trim() === '') return; // Skip adding empty todos
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: value,
+      done: false
+    };
+    todos = [...todos, newTodo];
+    value = ''; // Reset input value
+  }
+
+  function toggle(id) {
+    todos = todos.map(todo =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    );
+  }
+
+  function deleteTodo(id) {
+    todos = todos.filter(todo => todo.id !== id);
+  }
+
+
 </script>
+  <input type="text" bind:value={search} />
+  <select bind:value={showAll}>
+<option value={true}>all</option>
+<option value={false}>completed</option>
+</select>
 
 
-<Filter />
+<form on:submit|preventDefault={addTodo}>
+  <input type="text" bind:value={value} />
+</form>
 
-<Input />
-
-<List />
+<h1>Todos</h1>
+{#each todos as todo (todo.id)}
+{#if todo.title.includes(search) && (showAll || todo.done)}
+<div>
+  <button
+    style="text-decoration: {todo.done ? 'line-through' : 'none'}"
+    on:click={() => toggle(todo.id)}
+  >
+    {todo.title}
+  </button>
+  <button on:click={() => deleteTodo(todo.id)}>del</button>
+</div>
+{/if}
+{/each}
